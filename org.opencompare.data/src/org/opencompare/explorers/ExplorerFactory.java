42f69399b6dfdb4d12295c64502c61d274b8d3de
co-stig <setwindowtext@gmail.com>
2014-09-11 00:39:07 +0200

    Removed redundant files

--- org.opencompare.data/src/org/opencompare/explorers/ExplorerFactory.java
+++
@@ -1,9 +0,0 @@
-package org.opencompare.explorers;
-
-import org.opencompare.explore.ExplorationException;
-
-public interface ExplorerFactory {
-
-	Explorer getExplorer(String parent) throws ExplorationException;
-
-}
