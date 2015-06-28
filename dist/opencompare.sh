3412b665f450642879366267be1f0361e43e935a
Constantine <setwindowtext@gmail.com>
2015-06-28 02:05:19 +0200

    Tested on Debian 8, added shell script for executing on Linux

---
+++ dist/opencompare.sh
@@ -0,0 +1,2 @@
+export CLASSPATH=osgi/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar
+java org.eclipse.equinox.launcher.Main -configuration file:configuration/ -os linux -ws gtk -arch x86_64 -nl en_US
